5cb1834c5b2c6b94667c3dfe9c6364e1c3b6f20f
Ramu Ramasamy <rram.ramasamy@gmail.com>
2017-11-18 21:30:45 +0530

    password strength meter added

--- www/js/signup.js
+++ www/js/signup.js
@@ -1,4 +1,29 @@
 /**
+* checkPasswordStrength method checks the strength of the password
+*
+*/function checkPasswordStrength() {
+    var number = /([0-9])/;
+    var alphabets = /([a-zA-Z])/;
+    var special_characters = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/;
+    
+    if($('#user-password').val().length<6) {
+        $('#password-strength-status').removeClass();
+        $('#password-strength-status').addClass('weak-password');
+        $('#password-strength-status').html("Weak (should be atleast 6 characters.)");
+    } else {    
+        if($('#user-password').val().match(number) && $('#user-password').val().match(alphabets) && $('#password').val().match(special_characters)) {            
+            $('#password-strength-status').removeClass();
+            $('#password-strength-status').addClass('strong-password');
+            $('#password-strength-status').html("Strong");
+        } else {
+            $('#password-strength-status').removeClass();
+            $('#password-strength-status').addClass('medium-password');
+            $('#password-strength-status').html("Medium (should include alphabets, numbers and special characters.)");
+        } 
+    }
+}
+
+/**
 * saveUserInformation method collects the signup form data
 */
 function saveUserInformation(){
