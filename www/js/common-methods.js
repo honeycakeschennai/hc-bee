af36bfbee34f22fc81c453da4d1588eec821353f
Ramu Ramasamy <rram.ramasamy@gmail.com>
2017-11-18 10:31:01 +0530

    hc-token renamed as token

--- www/js/common-methods.js
+++ www/js/common-methods.js
@@ -1,7 +1,7 @@
 /**
 * gActiveToken holds the currently active token. It will be null if there's no value available for the key.
 */
-var gActiveToken = window.localStorage.getItem('hc-token');
+var gActiveToken = window.localStorage.getItem('token');
 
 /**
 * URLs
@@ -20,6 +20,6 @@ function logoutUser(){
 * cleanLocalStorage method cleans the local storage data.
 */
 function cleanLocalStorage(){
-	window.localStorage.removeItem('hc-token');
+	window.localStorage.removeItem('token');
 	window.localStorage.removeItem('userId');
 }
